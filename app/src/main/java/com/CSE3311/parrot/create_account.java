71952046ff662c1fdc0adf38cf38190820f08de0
dylanbaes <dylanbaes@gmail.com>
2022-09-26 19:33:55 -0500

    Create Account Activity Updated: removed navigation features to fix errors.

--- app/src/main/java/com/CSE3311/parrot/create_account.java
+++ app/src/main/java/com/CSE3311/parrot/create_account.java
@@ -2,20 +2,16 @@ package com.CSE3311.parrot;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
 
 import com.CSE3311.parrot.databinding.ActivityCreateAccountBinding;
 
 public class create_account extends AppCompatActivity {
 
-    private ActivityCreateAccountBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        ActivityCreateAccountBinding binding;
         super.onCreate(savedInstanceState);
 
         binding = ActivityCreateAccountBinding.inflate(getLayoutInflater());
