8a63539b891729dba058812815dbd0d0348d0c17
dylanbaes <dylanbaes@gmail.com>
2022-09-26 19:31:21 -0500

    Create Account Activity Updated: removed navigation features to fix errors.

--- app/src/main/java/com/CSE3311/parrot/create_account.java
+++ app/src/main/java/com/CSE3311/parrot/create_account.java
@@ -8,16 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import com.CSE3311.parrot.databinding.ActivityCreateAccountBinding;
 
 public class create_account extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityCreateAccountBinding binding;
 
     @Override
@@ -27,16 +21,6 @@ public class create_account extends AppCompatActivity {
         binding = ActivityCreateAccountBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_create_account);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        
-    }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_create_account);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
