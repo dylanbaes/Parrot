2dded2d3d02a2e8c80152af15d40fac8eb5c60e3
Nch2792 <nch2792@mavs.uta.edu>
2022-10-05 20:47:52 -0500

    Added a cancel button in create account and changed email and password variable names

--- app/src/main/java/com/CSE3311/parrot/create_account.java
+++ app/src/main/java/com/CSE3311/parrot/create_account.java
@@ -5,23 +5,26 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.parse.ParseException;
+import com.CSE3311.parrot.databinding.ActivityCreateAccountBinding;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
-import com.parse.Parse;
+
+import java.text.ParseException;
 
 public class create_account extends AppCompatActivity {
 
     EditText FirstName;
     EditText LastName;
-    EditText Email;
-    EditText Password;
+    EditText userEmailEditText;
+    EditText userPasswordEditText;
     Button userCreateAccount;
+    Button cancelbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,35 +37,51 @@ public class create_account extends AppCompatActivity {
 
         setContentView(R.layout.activity_create_account);
 
+        //Getting input for each edit text field
         FirstName = findViewById(R.id.firstname);
         LastName = findViewById(R.id.lastname);
-        Email = findViewById(R.id.email);
-        Password = findViewById(R.id.password);
+        userEmailEditText = findViewById(R.id.email);
+        userPasswordEditText = findViewById(R.id.password);
         userCreateAccount = findViewById(R.id.createaccountbtn);
+        cancelbutton = findViewById(R.id.cancelbtn);
 
+        //When the create account button is pressed
         userCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (!Email.getText().toString().isEmpty() && !Password.getText().toString().isEmpty()){
+            public void onClick(View v) {
+                if(!FirstName.getText().toString().isEmpty() && !LastName.getText().toString().isEmpty() && !userEmailEditText.getText().toString().isEmpty() && !userPasswordEditText.getText().toString().isEmpty())
+                {
                     ParseUser user = new ParseUser();
-                    user.setUsername(Email.getText().toString());
-                    user.setPassword(Password.getText().toString());
+                    user.setEmail(userEmailEditText.getText().toString());
+                    user.setPassword(userPasswordEditText.getText().toString());
+
                     user.signUpInBackground(new SignUpCallback() {
                         @Override
-                        public void done(ParseException e) {
+                        public void done(com.parse.ParseException e) {
                             if(e==null){
                                 Toast.makeText(getApplicationContext(),"Registration Successful!",Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(create_account.this, MainActivity.class));
                             }
                             else{
                                 Toast.makeText(getApplicationContext(),"Registration Failed!",Toast.LENGTH_LONG).show();
                             }
                         }
                     });
+
                 }
                 else
                 {
                     Toast.makeText(getApplicationContext(), "Missing Attributes", Toast.LENGTH_LONG).show();
                 }
+            }
+        });
+
+        cancelbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Go back to the login screen
+                startActivity(new Intent(create_account.this, Login.class));
             }
         });
     }
