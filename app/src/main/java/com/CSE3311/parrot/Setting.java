bddccaecb5790d75e90e9ced1024cedb5e6c6b40
Hoang Long Quan Nguyen <nguyenhoanglongquan@gmail.com>
2022-10-15 01:40:23 -0500

    Added setting page Added bottom navigation bar on home page and setting page Bottom navigation bar on home page:  - Notification icon is not functional yet  - Create icon (pencil icon) will redirect user to create entry page  - Update icon is not functional yet  - Setting icon will redirect user to setting page, from there user can logout  Bottom navigation bar on setting page:   - Notification icon is not functional yet   - Back icon will redirect user to home page

--- app/src/main/java/com/CSE3311/parrot/Setting.java
+++ app/src/main/java/com/CSE3311/parrot/Setting.java
@@ -1,70 +1,55 @@
 package com.CSE3311.parrot;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.parse.Parse;
 import com.parse.ParseUser;
 
+public class Setting extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-    //Button userCreateButton;
-    Button createEntryButton;
+    Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_setting);
+        logoutButton=findViewById(R.id.logoutButton);
 
-        //userCreateButton=findViewById(R.id.createaccountbtn);
-        createEntryButton=findViewById(R.id.createEntryButton);
-
-        createEntryButton.setOnClickListener(new View.OnClickListener() {
+        logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, activity_create_entry.class));
+                ParseUser.logOut();
+                startActivity(new Intent(Setting.this, Login.class));
             }
         });
+
         //Initialization for bottomNavigationView
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         bottomNavigationView.setSelectedItemId(R.id.setting);
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-                @Override
+            @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
-
-                    case R.id.notification:
-                        return true;
-                    case R.id.create:
-                        startActivity(new Intent(MainActivity.this, activity_create_entry.class));
+                    case R.id.back_from_setting:
+                        startActivity(new Intent(Setting.this, MainActivity.class));
                         overridePendingTransition(0,0);
                         return true;
-                    case R.id.update:
-                        return true;
-                    case R.id.setting:
-                        startActivity(new Intent(MainActivity.this, Setting.class));
-                        overridePendingTransition(0,0);
+                    case R.id.notification_from_setting:
                         return true;
                 }
                 return false;
             }
         });
-
-
-        //test
-        //ParseInstallation.getCurrentInstallation().saveInBackground();
 
     }
 }
