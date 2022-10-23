a49cb5c6b4fd9ce5fbe89257e425185849f7d75a
Nch2792 <nch2792@mavs.uta.edu>
2022-10-23 13:38:17 -0500

    Fixed notification error

--- app/src/main/java/com/CSE3311/parrot/notification_class.java
+++ app/src/main/java/com/CSE3311/parrot/notification_class.java
@@ -1,14 +1,18 @@
 package com.CSE3311.parrot;
 
 import android.app.Notification;
+import android.app.Notification.Builder;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.os.Build;
 import android.content.Intent;
 import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
-public class notification_class {
+public class notification_class extends AppCompatActivity {
     private final static String channel_id = "Parrot Notification";
 
     private void createNotificationChannel()
@@ -26,13 +30,23 @@ public class notification_class {
         }
     }
 
-    private Notification getNotification()
+    private void scheduleNotification()
+    {
+        Intent intent = new Intent(this, Login.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+    }
+
+
+    public Notification getNotification()
     {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channel_id)
                 .setContentTitle("ALERT!!")
                 .setContentText("You have a notification!")
                 .setStyle(new NotificationCompat.BigTextStyle()
                         .bigText("Longer text will not fit..."))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true);
+        return builder.build();
     }
 }
