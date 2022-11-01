1d8cdad321f7a45a41f7fb6c3047e95dcf6f15cf
joshburk <joshua.burkey@mavs.uta.edu>
2022-10-31 21:44:38 -0500

    starting in on encryption using virgil e3kit

---
+++ app/src/main/java/com/CSE3311/parrot/utils/AuthRx.kt
@@ -0,0 +1,30 @@
+package com.CSE3311.parrot.utils
+
+import com.parse.ParseCloud
+import io.reactivex.Single
+
+object AuthRx {
+
+    /**
+     * You can call it only after successful [authenticate]
+     */
+    fun virgilJwt(sessionToken: String) = Single.create<String> { emitter ->
+        val requestParams = mutableMapOf<String, String>().apply {
+            put("sessionToken", sessionToken)
+        }
+
+        ParseCloud.callFunctionInBackground<Map<String, Any>>(
+                KEY_VIRGIL_JWT,
+                requestParams
+        ) { virgilJwt, exception ->
+            if (exception == null)
+                emitter.onSuccess(virgilJwt[KEY_TOKEN].toString())
+            else
+                emitter.onError(exception)
+
+        }
+    }
+
+    private const val KEY_VIRGIL_JWT = "virgil-jwt"
+    private const val KEY_TOKEN = "token"
+}
