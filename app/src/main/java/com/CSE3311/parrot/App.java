94872e83231424e5157723911364688c850ab95b
Nch2792 <nch2792@mavs.uta.edu>
2022-11-08 17:17:36 -0600

    Updated notifications

--- app/src/main/java/com/CSE3311/parrot/App.java
+++ app/src/main/java/com/CSE3311/parrot/App.java
@@ -1,11 +1,30 @@
 package com.CSE3311.parrot;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 
 import com.parse.Parse;
 
 //initialize Parse SDK in Our App
 public class App extends Application{
+
+    private void createNotificationChannel()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(notification_class.channel_id, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -14,5 +33,7 @@ public class App extends Application{
                 .clientKey(getString(R.string.back4app_client_key))
                 .server(getString(R.string.back4app_server_url))
                 .build());
+
+        createNotificationChannel();
     }
 }
--- app/src/main/java/com/CSE3311/parrot/notification_class.java
+++ app/src/main/java/com/CSE3311/parrot/notification_class.java
@@ -1,52 +1,62 @@
 package com.CSE3311.parrot;
 
+import android.app.AlarmManager;
 import android.app.Notification;
-import android.app.Notification.Builder;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.os.Build;
-import android.content.Intent;
 import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class notification_class extends AppCompatActivity {
-    private final static String channel_id = "Parrot Notification";
+    public final static String channel_id = "Parrot Notification";
 
-    private void createNotificationChannel()
+    public void scheduleNotification(boolean OnGoing, String OnGoingOption)
     {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-        {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(channel_id, name, importance);
-            channel.setDescription(description);
-
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-
-    private void scheduleNotification()
-    {
-        Intent intent = new Intent(this, Login.class);
+        Intent intent = new Intent(this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.putExtra(channel_id, 1);
+        intent.putExtra(channel_id, getNotification());
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long time = SystemClock.elapsedRealtime();
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        assert alarmManager != null;
 
     }
 
-
-    public Notification getNotification()
+    @NonNull
+    private Notification getNotification()
     {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channel_id)
                 .setContentTitle("ALERT!!")
                 .setContentText("You have a notification!")
                 .setStyle(new NotificationCompat.BigTextStyle()
                         .bigText("Longer text will not fit..."))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setAutoCancel(true);
         return builder.build();
     }
+
+    public void showNotification()
+    {
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(1, getNotification());
+    }
+
 }
