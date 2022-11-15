72930fecee82fcc94a869c332da721742af4ce21
Cuuann <nicholasannguyen@gmail.com>
2022-11-15 15:33:37 -0600

    notification_class update

--- app/src/main/java/com/CSE3311/parrot/notification_class.java
+++ app/src/main/java/com/CSE3311/parrot/notification_class.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,41 +23,47 @@ import androidx.core.app.NotificationManagerCompat;
 
 public class notification_class extends AppCompatActivity {
     public final static String channel_id = "Parrot Notification";
+    private NotificationManagerCompat notificationManager;
+    private Button submitEntry;
 
-    public void scheduleNotification(boolean OnGoing, String OnGoingOption)
-    {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        intent.putExtra(channel_id, 1);
-        intent.putExtra(channel_id, getNotification());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_entry);
+        submitEntry = (Button) findViewById(R.id.create_entry_submit_submission);
 
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        long time = SystemClock.elapsedRealtime();
-
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        assert alarmManager != null;
-
+        notificationManager = NotificationManagerCompat.from(this);
     }
+    //submitEntry.setOnClickListener(new View.OnClickListener() {
 
-    @NonNull
-    private Notification getNotification()
-    {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channel_id)
-                .setContentTitle("ALERT!!")
-                .setContentText("You have a notification!")
+    public void Submit(View v) {
+
+       /*
+        Intent intent = new Intent(this, App.class);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        */
+
+        Notification builder = new NotificationCompat.Builder(this, channel_id)
+                .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+                .setContentTitle("Parrot")
+                .setContentText("You have successfully created an entry!")
+
+                //.setContentIntent(pendingIntent)
+                //have not implemented yet
+
+                //when notification sends it shows the big text first
+                //instead of contentText
+
                 .setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText("Longer text will not fit..."))
+                        .bigText("Your subscription is due next month"))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setAutoCancel(true);
-        return builder.build();
+                .setAutoCancel(true)
+                .build();
+
+        notificationManager.notify(1, builder);
+
+
     }
-
-    public void showNotification()
-    {
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-        notificationManager.notify(1, getNotification());
-    }
-
 }
