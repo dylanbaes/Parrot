9a72382ae1e5694f62f1ce56786fe9227141f6ce
dylanbaes <dylanbaes@gmail.com>
2022-12-04 21:52:17 -0600

    View/Update List Preparation

---
+++ app/src/main/java/com/CSE3311/parrot/ViewExpense.java
@@ -0,0 +1,39 @@
+package com.CSE3311.parrot;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewExpense extends AppCompatActivity {
+
+    private TextView categoryName;
+    private TextView description;
+    private TextView paymentType;
+    private TextView categoryType;
+    private TextView startDate;
+    private TextView endDate;
+    private TextView notificationType;
+    private TextView notificationDate;
+    private TextView cost;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.entry_view_expense);
+
+        categoryName = findViewById(R.id.category_name);
+        description = findViewById(R.id.description);
+        paymentType = findViewById(R.id.payment_amount);
+        categoryType = findViewById(R.id.payment_type);
+        startDate = findViewById(R.id.start_date);
+        endDate = findViewById(R.id.end_date);
+        notificationType = findViewById(R.id.notification_type);
+        notificationDate = findViewById(R.id.notification_date);
+        cost = findViewById(R.id.cost);
+
+
+    }
+}
--- app/src/main/java/com/CSE3311/parrot/ViewIncome.java
+++ app/src/main/java/com/CSE3311/parrot/ViewIncome.java
@@ -7,32 +7,25 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ViewItem extends AppCompatActivity {
+public class ViewIncome extends AppCompatActivity {
 
-    private TextView categoryName;
+    private TextView incomeName; // part-time, full-time ...
     private TextView description;
-    private TextView paymentType;
-    private TextView categoryType;
-    private TextView startDate;
-    private TextView endDate;
-    private TextView notificationType;
+    private TextView paymentAmount;
+    private TextView paymentType; // biweekly or monthly
+    private TextView paymentDate;
     private TextView notificationDate;
-    private TextView cost;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.entry_view);
+        setContentView(R.layout.entry_view_income);
 
-        categoryName = findViewById(R.id.category_name);
+        incomeName = findViewById(R.id.income_name);
         description = findViewById(R.id.description);
+        paymentAmount = findViewById(R.id.payment_amount);
         paymentType = findViewById(R.id.payment_type);
-        categoryType = findViewById(R.id.category_type);
-        startDate = findViewById(R.id.start_date);
-        endDate = findViewById(R.id.end_date);
-        notificationType = findViewById(R.id.notification_type);
         notificationDate = findViewById(R.id.notification_date);
-        cost = findViewById(R.id.cost);
 
 
     }
